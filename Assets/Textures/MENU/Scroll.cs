8b31198091d40331f9d55e905373658baba7ea1a
Borys <Borys.Duchewicz@op.pl>
2020-02-13 23:47:22 +0100

    Płynnie przesuwające się elementy w menu
    Trzeba ustawić i dać dobre grafiki czyli 0/1

---
+++ Assets/Textures/MENU/Scroll.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour
+{
+    public float speed = 0.5f;
+    private Renderer renderer;
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+   
+    void Update()
+    {
+        Vector2 offset = new Vector2(0,Time.time * speed);
+        renderer.material.mainTextureOffset = offset;
+    }
+}
