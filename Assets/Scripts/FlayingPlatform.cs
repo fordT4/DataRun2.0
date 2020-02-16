f34042f9e8b06496c45db923294fcf1f69fa40d1
Borys <Borys.Duchewicz@op.pl>
2020-02-16 18:00:33 +0100

    W pełni poprawiony poziom 3

--- Assets/Scripts/FlayingPlatform.cs
+++ Assets/Scripts/FlayingPlatform.cs
@@ -5,32 +5,16 @@ using Random = UnityEngine.Random;
 public class FlayingPlatform : MonoBehaviour
 {
     private Rigidbody2D rb;
-    /*[SerializeField] public GameObject myPrefabs;
-    private Vector3 position;*/
-
-  
-
+    
     void Start()
     {
-        
         rb = GetComponent<Rigidbody2D>();
-        /*position = transform.position;
-        StartCoroutine(Spawn());*/
+      
     }
-  
-    /*private IEnumerator Spawn()
-    {
-        
-        rb.velocity = new Vector2(0, 5f);
-        yield return new WaitForSeconds(Random.Range(1.5f, 5f));
-        Instantiate(myPrefabs, position, Quaternion.identity);
-
-    }*/
-    // Update is called once per frame
     void Update()
     {
         rb.velocity = new Vector2(0, 5f);
-        if (rb.transform.position.y > 68)
+        if (rb.transform.position.y > 68 || !CameraControler.change)
         {
             Destroy(gameObject);
         }
