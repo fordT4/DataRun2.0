2e5d491fe2b48209b46b0849f8cc93846263def1
Borys <Borys.Duchewicz@op.pl>
2020-02-23 18:52:44 +0100

    Rocket

--- Assets/Scripts/Mechanics/Rocket.cs
+++ Assets/Scripts/Mechanics/Rocket.cs
@@ -10,25 +10,21 @@ public class Rocket : MonoBehaviour
     public LayerMask layerMask;
     private PlayerController target;
     private Vector2 moveDirection;
-   
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         target = GameObject.FindObjectOfType<PlayerController>();
         moveDirection = (target.transform.position - transform.position).normalized * moveSpeed;
-        rb.velocity = new Vector2(moveDirection.x, moveDirection.y);
-        Destroy(gameObject,3f);
-       
-    }
-    void Update()
-    {
-      
-        RaycastHit2D groundInfo = Physics2D.Raycast(transform.position, Vector2.down, 0.3f, layerMask);
-        if (groundInfo.collider)
+        Debug.Log(moveDirection);
+        if (moveDirection.x < 0)
         {
-            
-            Destroy(gameObject);
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
         }
+        rb.velocity = new Vector2(moveDirection.x, moveDirection.y);
+
     }
     private void OnTriggerEnter2D(Collider2D coll)
     {
@@ -36,10 +32,14 @@ public class Rocket : MonoBehaviour
         {
            
             target.Reject(transform.position);
-            Destroy(gameObject);
+            Destroy(gameObject,0.1f);
         }
-
+        if (coll.gameObject.tag == "floor" || coll.gameObject.tag == "wall")
+        {
+            Destroy(gameObject, 0.1f);
+        }
     }
+
 
    
 
